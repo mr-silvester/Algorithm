59fd4d9fecb93ef6e0390a3219fe9e1755c060fe
John Doe <126128590+mr-silvester@users.noreply.github.com>
2025-02-03 20:44:47 +0900

    [Bronze V] Title: N 찍기, Time: 800 ms, Memory: 31480 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = intNum(br);
+        
+        br.close();
+        
+        // solution;
+        solution(N);
+    }
+    
+    private static void solution(int N) {
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+    
+    private static void print(int n) {
+        System.out.print(n);
+    }
+    
+    private static void print(int[] arr) {
+        StringBuilder sb = new StringBuilder(arr.length * 2);
+        
+        for (int n : arr) {
+            sb.append(n).append(" ");
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    private static void print(long[] arr) {
+        StringBuilder sb = new StringBuilder(arr.length * 2);
+        
+        for (long n : arr) {
+            sb.append(n).append(" ");
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    private static void println(long[] arr) {
+        for (long n : arr) {
+            System.out.println(n);
+        }
+    }
+    
+    private static void print(String[] arr) {
+        StringBuilder sb = new StringBuilder(arr.length * 2);
+        
+        for (String s : arr) {
+            sb.append(s).append(" ");
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    
+    private static String[] strArr(BufferedReader br) throws IOException {
+        return br.readLine().split(" ");
+    }
+    
+    private static int intNum(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+    
+    private static int[] intArr(BufferedReader br) throws IOException {
+        String[] input = br.readLine().split(" ");
+        
+        int[] result = new int[input.length];
+        
+        for (int i = 0; i < input.length; i++) {
+            result[i] = Integer.parseInt(input[i]);
+        }
+        
+        return result;
+    }
+    
+    private static long longNum(BufferedReader br) throws IOException {
+        return Long.parseLong(br.readLine());
+    }
+    
+    private static long[] longArr(BufferedReader br) throws IOException {
+        String[] input = br.readLine().split(" ");
+        
+        long[] result = new long[input.length];
+        
+        for (int i = 0; i < input.length; i++) {
+            result[i] = Long.parseLong(input[i]);
+        }
+        
+        return result;
+    }
+}
