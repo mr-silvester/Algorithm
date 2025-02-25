37fd7989326000ff3e1b5cf0a04572b99405882a
John Doe <126128590+mr-silvester@users.noreply.github.com>
2025-02-25 20:05:48 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1328 ms, Memory: 74688 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,123 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int[] NM = intArr(br);
+        
+        int N = NM[0];
+        
+        int M = NM[1];
+        
+        int[] arr = intArr(br);
+        
+        int[][] ranges = new int[M][];
+        
+        for (int i = 0; i < M; i++) {
+            ranges[i] = intArr(br);
+        }
+        
+        br.close();
+        
+        // solution;
+        solution(N, M, arr, ranges);
+    }
+    
+    private static void solution(int N, int M, int[] arr, int[][] ranges) {
+        int[] sum = new int[N + 1];
+        
+        for (int i = 0; i < N; i++) {
+            sum[i + 1] = sum[i] + arr[i];
+        }
+        
+        for (int[] range : ranges) {
+            int i = range[0];
+            
+            int j = range[1];
+            
+            int val = sum[j] - sum[i - 1];
+            
+            System.out.println(val);
+        }
+    }
+    
+    private static void print(int n) {
+        System.out.print(n);
+    }
+    
+    private static void print(int[] arr) {
+        StringBuilder sb = new StringBuilder(arr.length * 2);
+        
+        for (int n : arr) {
+            sb.append(n).append(" ");
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    private static void print(long[] arr) {
+        StringBuilder sb = new StringBuilder(arr.length * 2);
+        
+        for (long n : arr) {
+            sb.append(n).append(" ");
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    private static void println(long[] arr) {
+        for (long n : arr) {
+            System.out.println(n);
+        }
+    }
+    
+    private static void print(String[] arr) {
+        StringBuilder sb = new StringBuilder(arr.length * 2);
+        
+        for (String s : arr) {
+            sb.append(s).append(" ");
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    
+    private static String[] strArr(BufferedReader br) throws IOException {
+        return br.readLine().split(" ");
+    }
+    
+    private static int intNum(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+    
+    private static int[] intArr(BufferedReader br) throws IOException {
+        String[] input = br.readLine().split(" ");
+        
+        int[] result = new int[input.length];
+        
+        for (int i = 0; i < input.length; i++) {
+            result[i] = Integer.parseInt(input[i]);
+        }
+        
+        return result;
+    }
+    
+    private static long longNum(BufferedReader br) throws IOException {
+        return Long.parseLong(br.readLine());
+    }
+    
+    private static long[] longArr(BufferedReader br) throws IOException {
+        String[] input = br.readLine().split(" ");
+        
+        long[] result = new long[input.length];
+        
+        for (int i = 0; i < input.length; i++) {
+            result[i] = Long.parseLong(input[i]);
+        }
+        
+        return result;
+    }
+}
